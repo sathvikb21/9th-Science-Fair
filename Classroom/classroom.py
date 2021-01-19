15152f6079df1b97e5866d3410ca75b318c2f6b3
Sathvik Bhavaraju <sathvikb21@gmail.com>
2021-01-19 11:07:25 -0500

    Self issue

--- Classroom/classroom.py
+++ Classroom/classroom.py
@@ -4,45 +4,36 @@ import os.path
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
+from googleapiclient import errors
 
-# If modifying these scopes, delete the file token.pickle.
-SCOPES = ['https://www.googleapis.com/auth/classroom.courses.readonly']
 
-def main():
-    """Shows basic usage of the Classroom API.
-    Prints the names of the first 10 courses the user has access to.
-    """
-    creds = None
-    # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
-            creds = pickle.load(token)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
+class ClassroomSnippets(object):
+    def __init__(self, service):
+        self.service = service
 
-    service = build('classroom', 'v1', credentials=creds)
-
-    # Call the Classroom API
-    results = service.courses().list(pageSize=10).execute()
-    courses = results.get('courses', [])
-
-    if not courses:
-        print('No courses found.')
-    else:
-        print('Courses:')
-        for course in courses:
-            print(course['name'])
+    def add_student(self, course_id):
+        """ Adds a student to a course. """
+        service = self.service
+        # [START classroom_add_student]
+        enrollment_code = 'grc4obp'
+        student = {
+            'userId': 'tertuserr@gmail.com'
+        }
+        try:
+            student = service.courses().students().create(
+                courseId=course_id,
+                enrollmentCode=enrollment_code,
+                body=student).execute()
+            print(
+                '''User {%s} was enrolled as a student in
+                   the course with ID "{%s}"'''
+                % (student.get('profile').get('name').get('fullName'),
+                   course_id))
+        except errors.HttpError as error:
+            print('You are already a member of this course.')
+        # [END classroom_add_student]
+            return error
+        return student
 
 if __name__ == '__main__':
-    main()
+    ClassroomSnippets.add_student('233667426861', '233667426861')
